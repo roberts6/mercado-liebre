68ea69ef47fa466e9d502013fc053f0457d77b38
roberts6 <roberts_oscar@hotmail.com>
2021-01-19 17:50:14 -0600

    reemplazo de index por home y estilos por styles

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ const publicPath = path.resolve(__dirname, "./mercado-liebre");
 app.listen(3666, () => console.log("Funcionando en el puerto 3666"));
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "views/index.html")
+    res.sendFile(path.join(__dirname, "views/home.html")
     )});
